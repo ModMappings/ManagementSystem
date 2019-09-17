253c8a65c14ecdbafef60aee214d74218718bc8c
Marc Hermans <oriondevelopment@outlook.com>
2019-09-17 20:02:41 +0200

    Getting some auth to work.

--- Auth.Admin.Api/Program.cs
+++ Auth.Admin.Api/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Auth.Admin.Api
 {
@@ -10,8 +14,43 @@ namespace Auth.Admin.Api
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var applicationName = Assembly.GetEntryAssembly()?.GetName().Name;
+
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "..", "config", $"global.json"), true, true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "..", "config", $"global{env.EnvironmentName}.json"), true,
+                        true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "..", "config", $"{applicationName}.json"),
+                        true, true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "..", "config",
+                            $"{applicationName}.{env.EnvironmentName}.json"), true, true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"global.json"), true, true);
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"global{env.EnvironmentName}.json"),
+                        true, true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"{applicationName}.json"), true,
+                        true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "config", $"{applicationName}.{env.EnvironmentName}.json"),
+                        true, true);
+
+                    config.AddEnvironmentVariables();
+                })
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration.ReadFrom.Configuration(context.Configuration, "Logging");
+                })
                 .UseStartup<Startup>();
+        }
     }
 }
--- Auth.Admin/Program.cs
+++ Auth.Admin/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -6,6 +8,7 @@ using Serilog;
 using Auth.Admin.EntityFramework.Shared.DbContexts;
 using Auth.Admin.EntityFramework.Shared.Entities.Identity;
 using Auth.Admin.Helpers;
+using Microsoft.Extensions.Configuration;
 
 namespace Auth.Admin
 {
@@ -25,16 +28,51 @@ namespace Auth.Admin
             if (seed)
             {
                 await DbMigrationHelpers.EnsureSeedData<IdentityServerConfigurationDbContext, AdminIdentityDbContext, IdentityServerPersistedGrantDbContext, AdminLogDbContext, UserIdentity, UserIdentityRole>(host);
+                return;
             }
 
             host.Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                   .UseKestrel(c => c.AddServerHeader = false)
-                   .UseStartup<Startup>()
-                   .UseSerilog()
-                   .Build();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var applicationName = Assembly.GetEntryAssembly()?.GetName().Name;
+            
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(c => c.AddServerHeader = false)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "..", "config", $"global.json"), true, true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "..", "config", $"global{env.EnvironmentName}.json"), true,
+                        true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "..", "config", $"{applicationName}.json"),
+                        true, true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "..", "config",
+                            $"{applicationName}.{env.EnvironmentName}.json"), true, true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"global.json"), true, true);
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"global{env.EnvironmentName}.json"),
+                        true, true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"{applicationName}.json"), true,
+                        true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "config", $"{applicationName}.{env.EnvironmentName}.json"),
+                        true, true);
+
+                    config.AddEnvironmentVariables();
+                })
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration.ReadFrom.Configuration(context.Configuration, "Logging");
+                })
+                .Build();
+        }
     }
 }
