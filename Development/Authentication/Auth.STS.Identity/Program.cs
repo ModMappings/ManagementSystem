5f9e432051142e28bdd8ae32aabbef17c688ec4c
Marc Hermans <oriondevelopment@outlook.com>
2019-09-22 16:38:43 +0200

    Fixing logging issues.

--- Development/Authentication/Auth.STS.Identity/Program.cs
+++ Development/Authentication/Auth.STS.Identity/Program.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Serilog;
 
 namespace Auth.STS.Identity
@@ -13,8 +16,44 @@ namespace Auth.STS.Identity
 				.Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)               
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var applicationName = Assembly.GetEntryAssembly()?.GetName().Name;
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseIIS()
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "..", "config", $"global.json"), true, true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "..", "config", $"global{env.EnvironmentName}.json"), true,
+                        true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "..", "config", $"{applicationName}.json"),
+                        true, true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "..", "config",
+                            $"{applicationName}.{env.EnvironmentName}.json"), true, true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"global.json"), true, true);
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"global{env.EnvironmentName}.json"),
+                        true, true);
+
+                    config.AddJsonFile(Path.Combine(env.ContentRootPath, "config", $"{applicationName}.json"), true,
+                        true);
+                    config.AddJsonFile(
+                        Path.Combine(env.ContentRootPath, "config", $"{applicationName}.{env.EnvironmentName}.json"),
+                        true, true);
+
+                    config.AddEnvironmentVariables();
+                })
+                .UseSerilog((context, configuration) =>
+                {
+                    configuration.ReadFrom.Configuration(context.Configuration, "Logging");
+                })
                 .UseStartup<Startup>();
+        }
     }
 }
