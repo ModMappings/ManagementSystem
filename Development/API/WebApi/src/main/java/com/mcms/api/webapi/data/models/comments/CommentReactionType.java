3e5be8025f5897187fa7fd7175489bfcabd0ccb9
Marc Hermans <oriondevelopment@outlook.com>
2020-01-05 09:19:06 +0100

    feat(datalayer): Started work on porting the DMOs.

---
+++ Development/API/WebApi/src/main/java/com/mcms/api/webapi/data/models/comments/CommentReactionType.java
@@ -0,0 +1,43 @@
+package com.mcms.api.webapi.data.models.comments;
+
+public enum CommentReactionType {
+    /**
+     * :+1:
+     */
+    PLUS_ONE,
+
+    /**
+     * :-1:
+     */
+    MINUS_ONE,
+
+    /**
+     * :smile:
+     */
+    SMILE,
+
+    /**
+     * :confused:
+     */
+    CONFUSED,
+
+    /**
+     * :heart:
+     */
+    HEART,
+
+    /**
+     * :tada:
+     */
+    HOORAY,
+
+    /**
+     * :rocket:
+     */
+    ROCKET,
+
+    /**
+     * :eyes:
+     */
+    EYES
+}
