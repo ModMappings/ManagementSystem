7748a3aad995bdfd593af1039d8347b10112265d
Marc Hermans <oriondevelopment@outlook.com>
2020-01-05 16:24:59 +0100

    feat(data): Added the IReleaseComponentRepository.

---
+++ Development/API/Data/Core/src/main/java/com/mcms/data/core/repositories/core/release/IReleaseComponentRepository.java
@@ -0,0 +1,29 @@
+package com.mcms.data.core.repositories.core.release;
+
+import java.util.UUID;
+
+import com.mcms.api.datamodel.core.release.ReleaseComponentDMO;
+import com.mcms.data.core.repositories.IRepository;
+import reactor.core.publisher.Flux;
+
+/**
+ * Represents a repository which can provide and store {@link ReleaseComponentDMO} objects.
+ */
+public interface IReleaseComponentRepository extends IRepository<ReleaseComponentDMO> {
+
+    /**
+     * Finds all release component which are part of a given release that has the given id.
+     *
+     * @param releaseId The id of the release that the components are being looked up for.
+     * @return The release components which are part of the release with the given id.
+     */
+    Flux<ReleaseComponentDMO> findAllForRelease(UUID releaseId);
+
+    /**
+     * Finds all release component which target a mapping with the given id.
+     *
+     * @param mappingId The id of the mapping that the components are being looked up for.
+     * @return The release components which target the mapping with the given id.
+     */
+    Flux<ReleaseComponentDMO> findAllForMapping(UUID mappingId);
+}
