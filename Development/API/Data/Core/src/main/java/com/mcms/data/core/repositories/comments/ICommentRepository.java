0e5a82a2b4102b732ae3b4c1a67f0ac5dedc7cdb
Marc Hermans <oriondevelopment@outlook.com>
2020-01-05 15:59:17 +0100

    feat(data): Added the ICommentReactionRepository.

--- Development/API/Data/Core/src/main/java/com/mcms/data/core/repositories/comments/ICommentRepository.java
+++ Development/API/Data/Core/src/main/java/com/mcms/data/core/repositories/comments/ICommentRepository.java
@@ -14,6 +14,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
     /**
      * Finds all comments which where directly made on the release with the given id.
      *
+     * The comments are sorted oldest to newest.
+     *
      * @param releaseId The id of the release to look up comments for.
      * @return The comments on the given release.
      * @throws IllegalArgumentException in case the given {@literal releaseId} is {@literal null}.
@@ -22,6 +24,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
 
     /**
      * Finds all comments which where directly made on the releases with the given ids.
+     *
+     * The comments are sorted oldest to newest.
      *
      * @param releaseIds The ids of the releases to look up comments for.
      * @return The comments on the given releases.
@@ -32,6 +36,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
     /**
      * Finds all comments which where directly made on the releases with the given ids.
      *
+     * The comments are sorted oldest to newest.
+     *
      * @param releaseIds The ids of the releases to look up comments for.
      * @return The comments on the given releases.
      * @throws IllegalArgumentException in case the given {@literal releaseIds} is {@literal null}.
@@ -40,6 +46,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
 
     /**
      * Finds all comments which where directly made on the proposed mapping with the given id.
+     *
+     * The comments are sorted oldest to newest.
      *
      * @param proposedMappingId The id of the proposed mapping to look up comments for.
      * @return The comments on the given proposed mapping.
@@ -50,6 +58,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
     /**
      * Finds all comments which where directly made on the proposed mappings with the given ids.
      *
+     * The comments are sorted oldest to newest.
+     *
      * @param proposedMappingIds The ids of the proposed mappings to look up comments for.
      * @return The comments on the given proposed mappings.
      * @throws IllegalArgumentException in case the given {@literal proposedMappingIds} is {@literal null}.
@@ -58,6 +68,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
 
     /**
      * Finds all comments which where directly made on the proposed mappings with the given ids.
+     *
+     * The comments are sorted oldest to newest.
      *
      * @param proposedMappingIds The ids of the proposed mappings to look up comments for.
      * @return The comments on the given proposed mappings.
@@ -68,6 +80,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
     /**
      * Finds all comments which where directly made on the comment with the given id.
      *
+     * The comments are sorted oldest to newest.
+     *
      * @param commentId The id of the comment to look up comments for.
      * @return The comments on the given comment.
      * @throws IllegalArgumentException in case the given {@literal commentId} is {@literal null}.
@@ -76,6 +90,8 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
 
     /**
      * Finds all comments which where directly made on the comments with the given ids.
+     *
+     * The comments are sorted oldest to newest.
      *
      * @param commentIds The ids of the comments to look up comments for.
      * @return The comments on the given comments.
@@ -86,9 +102,44 @@ public interface ICommentRepository extends IRepository<CommentDMO> {
     /**
      * Finds all comments which where directly made on the comments with the given ids.
      *
+     * The comments are sorted oldest to newest.
+     *
      * @param commentIds The ids of the comments to look up comments for.
      * @return The comments on the given comments.
      * @throws IllegalArgumentException in case the given {@literal commentIds} is {@literal null}.
      */
     Flux<CommentDMO> findAllForComments(final Publisher<UUID> commentIds);
+
+    /**
+     * Finds all comments which where directly made by the user with the given id.
+     *
+     * The comments are sorted newest to oldest.
+     *
+     * @param userId The id of the user to look up comments for.
+     * @return The comments by the given user.
+     * @throws IllegalArgumentException in case the given {@literal userId} is {@literal null}.
+     */
+    Flux<CommentDMO> findAllForUser(final UUID userId);
+
+    /**
+     * Finds all comments which where directly made by the users with the given ids.
+     *
+     * The comments are sorted newest to oldest.
+     *
+     * @param userIds The ids of the users to look up comments for.
+     * @return The comments by the given users.
+     * @throws IllegalArgumentException in case the given {@literal userIds} is {@literal null}.
+     */
+    Flux<CommentDMO> findAllForUsers(final Iterable<UUID> userIds);
+
+    /**
+     * Finds all comments which where directly made by the users with the given ids.
+     *
+     * The comments are sorted newest to oldest.
+     *
+     * @param userIds The ids of the users to look up comments for.
+     * @return The comments by the given users.
+     * @throws IllegalArgumentException in case the given {@literal userIds} is {@literal null}.
+     */
+    Flux<CommentDMO> findAllForUsers(final Publisher<UUID> userIds);
 }
