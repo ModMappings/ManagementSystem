f30151f14004ee82fad371c4bc2266ea0f348359
Marc <marc.hermans@ldtteam.com>
2020-01-06 20:32:42 +0100

    feat(docu): Add documentation to the IMappableInGameVersionInheritanceDataRepository.

--- Development/API/Data/Core/src/main/java/com/mcms/data/core/repositories/mapping/mappable/IMappableInGameVersionInheritanceDataRepository.java
+++ Development/API/Data/Core/src/main/java/com/mcms/data/core/repositories/mapping/mappable/IMappableInGameVersionInheritanceDataRepository.java
@@ -6,9 +6,26 @@ import com.mcms.api.datamodel.mapping.mappable.MappableInGameVersionInheritanceD
 import com.mcms.data.core.repositories.IRepository;
 import reactor.core.publisher.Flux;
 
+/**
+ * Represents a repository which can provide and store {@link MappableInGameVersionInheritanceDataDMO} objects.
+ */
 public interface IMappableInGameVersionInheritanceDataRepository extends IRepository<MappableInGameVersionInheritanceDataDMO> {
 
+    /**
+     * Finds all the inheritance data in which the given mappable in game version class is
+     * the super type tole.
+     *
+     * @param superTypeMappableInGameVersionId The id of the mappable in game version class for which the inheritance data in super type role will be looked up.
+     * @return All inheritance data which indicates that the given mappable in a game version is a super type.
+     */
     Flux<MappableInGameVersionInheritanceDataDMO> findAllForSuperType(UUID superTypeMappableInGameVersionId);
 
+    /**
+     * Finds all the inheritance data in which the given mappable in game version class is
+     * the sub type tole.
+     *
+     * @param subTypeMappableInGameVersionId The id of the mappable in game version class for which the inheritance data in sub type role will be looked up.
+     * @return All inheritance data which indicates that the given mappable in a game version is a sub type.
+     */
     Flux<MappableInGameVersionInheritanceDataDMO> findAllForSubType(UUID subTypeMappableInGameVersionId);
 }
