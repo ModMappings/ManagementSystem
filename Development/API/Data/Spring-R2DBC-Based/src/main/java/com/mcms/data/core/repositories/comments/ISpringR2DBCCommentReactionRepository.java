5bb6ef3d89f408981eb9c72053d48a07069d7422
Marc Hermans <oriondevelopment@outlook.com>
2020-01-10 18:08:41 +0100

    feat(data): Fixing the Spring R2DBC impl for CommentReactions.

--- Development/API/Data/Spring-R2DBC-Based/src/main/java/com/mcms/data/core/repositories/comments/ISpringR2DBCCommentReactionRepository.java
+++ Development/API/Data/Spring-R2DBC-Based/src/main/java/com/mcms/data/core/repositories/comments/ISpringR2DBCCommentReactionRepository.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import com.mcms.api.datamodel.comments.CommentReactionDMO;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 /**
@@ -15,11 +16,11 @@ import reactor.core.publisher.Flux;
  */
 public interface ISpringR2DBCCommentReactionRepository extends ICommentReactionRepository {
 
-    @Query("Select * from comment_reaction cr where cr.commentReactionId = :commentReactionId")
+    @Query("Select * from comment_reaction cr where cr.commentReactionId = $1")
     @Override
     Flux<CommentReactionDMO> findAllForComment(final UUID commentReactionId, final Pageable pageable);
 
-    @Query("Select * from comment_reaction cr where cr.createdBy = :userId")
+    @Query("Select * from comment_reaction cr where cr.createdBy = $1")
     @Override
     Flux<CommentReactionDMO> findAllForUser(final UUID userId, final Pageable pageable);
 }
