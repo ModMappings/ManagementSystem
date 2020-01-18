1939a0e6f77b1df3bdc2cb41a784455eecc2e01d
Marc Hermans <marc.hermans@ldtteam.com>
2020-01-18 22:23:40 +0100

    Attempting to get a token out of keycloak.

--- source/api/src/main/java/org/modmappings/mmms/api/configuration/OpenApiConfiguration.java
+++ source/api/src/main/java/org/modmappings/mmms/api/configuration/OpenApiConfiguration.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.OAuthFlow;
 import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,19 +21,43 @@ public class OpenApiConfiguration {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("ModMappings Official auth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
+                                .type(SecurityScheme.Type.OPENIDCONNECT)
                                 .scheme("bearer")
+                                .in(SecurityScheme.In.HEADER)
                                 .bearerFormat("JWT")
+                                .description("The official OpenID connect authentication server for ModMappings.")
+                                .openIdConnectUrl("https://testauth.minecraftforge.net/realms/ModMappings/.well-known/openid-configuration")
                                 .flows(new OAuthFlows()
-                                        .authorizationCode(new OAuthFlow())
+                                        .implicit(
+                                                new OAuthFlow()
+                                                        .authorizationUrl("https://testauth.minecraftforge.net/realms/ModMappings/protocol/openid-connect/auth")
+                                                        .tokenUrl("https://testauth.minecraftforge.net/realms/ModMappings/protocol/openid-connect/token")
+                                                        .scopes(new Scopes()
+                                                                .addString("GAMEVERSIONS_DELETE", "Allows for game version deletion")
+                                                                .addString("GAMEVERSIONS_UPDATE", "Allows for game version updating")
+                                                                .addString("GAMEVERSIONS_CREATE", "Allows for game version creation")
+                                                        )
+                                        )
                                 )
                         )
                         .addSecuritySchemes("ModMappings Local development auth", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
+                                .type(SecurityScheme.Type.OPENIDCONNECT)
                                 .scheme("bearer")
+                                .in(SecurityScheme.In.HEADER)
                                 .bearerFormat("JWT")
+                                .description("The local development OpenID connect authentication server for ModMappings.")
+                                .openIdConnectUrl("http://localhost:8081/realms/ModMappings/.well-known/openid-configuration")
                                 .flows(new OAuthFlows()
-                                        .authorizationCode(new OAuthFlow())
+                                    .implicit(
+                                            new OAuthFlow()
+                                                .authorizationUrl("http://localhost:8081/realms/ModMappings/protocol/openid-connect/auth")
+                                                .tokenUrl("http://localhost:8081/realms/ModMappings/protocol/openid-connect/token")
+                                                .scopes(new Scopes()
+                                                        .addString("GAMEVERSIONS_DELETE", "Allows for game version deletion")
+                                                        .addString("GAMEVERSIONS_UPDATE", "Allows for game version updating")
+                                                        .addString("GAMEVERSIONS_CREATE", "Allows for game version creation")
+                                                )
+                                    )
                                 )
                         )
                 )
