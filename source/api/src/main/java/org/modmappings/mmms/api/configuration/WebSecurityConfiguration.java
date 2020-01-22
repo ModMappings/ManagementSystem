fd08340a492f47ce9fa29e00c66c01d5ae8ad2e3
Marc Hermans <oriondevelopment@outlook.com>
2020-01-22 18:02:56 +0100

    Some minor clean up with regards to the WebSecurityConfiguration.

--- source/api/src/main/java/org/modmappings/mmms/api/configuration/WebSecurityConfiguration.java
+++ source/api/src/main/java/org/modmappings/mmms/api/configuration/WebSecurityConfiguration.java
@@ -1,33 +1,22 @@
 package org.modmappings.mmms.api.configuration;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
-import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.stream.Collectors;
 
 @Configuration
