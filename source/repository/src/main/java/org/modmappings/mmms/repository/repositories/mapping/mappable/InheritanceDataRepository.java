02878e7d5ad37843e008fd2591eeaa049f0c1b06
Marc Hermans <marc.hermans@ldtteam.com>
2020-01-26 12:06:34 +0100

    Porting the InheritanceDataRepository to the new system.
    Signed-off-by: Marc Hermans <marc.hermans@ldtteam.com>

---
+++ source/repository/src/main/java/org/modmappings/mmms/repository/repositories/mapping/mappable/InheritanceDataRepository.java
@@ -0,0 +1,63 @@
+package org.modmappings.mmms.repository.repositories.mapping.mappable;
+
+import java.util.UUID;
+
+import org.modmappings.mmms.er2dbc.data.access.strategy.ExtendedDataAccessStrategy;
+import org.modmappings.mmms.repository.model.mapping.mappable.InheritanceDataDMO;
+import org.modmappings.mmms.repository.repositories.ModMappingR2DBCRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.r2dbc.convert.R2dbcConverter;
+import org.springframework.data.r2dbc.core.DatabaseClient;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.relational.repository.query.RelationalEntityInformation;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.modmappings.mmms.er2dbc.data.statements.criteria.ColumnBasedCriteria.*;
+
+/**
+ * Represents a repository which can provide and store {@link InheritanceDataDMO} objects.
+ */
+public class InheritanceDataRepository extends ModMappingR2DBCRepository<InheritanceDataDMO> {
+
+    public InheritanceDataRepository(RelationalEntityInformation<InheritanceDataDMO, UUID> entity, DatabaseClient databaseClient, R2dbcConverter converter, ExtendedDataAccessStrategy accessStrategy) {
+        super(entity, databaseClient, converter, accessStrategy);
+    }
+
+    /**
+     * Finds all the inheritance data in which the given versioned mappable class is
+     * the super type tole.
+     *
+     * @param superTypeVersionedMappableId The id of the versioned mappable class for which the inheritance data in super type role will be looked up.
+     * @param pageable The pageable information in the request.
+     * @return All inheritance data which indicates that the given mappable in a game version is a super type.
+     */
+    public Mono<Page<InheritanceDataDMO>> findAllForSuperType(
+            final UUID superTypeVersionedMappableId,
+            final Pageable pageable)
+    {
+       return createPagedStarRequest(
+               selectSpecWithJoin -> selectSpecWithJoin.withCriteria(where(reference("super_type_versioned_mappable_id")).is(parameter(superTypeVersionedMappableId))),
+               pageable
+       );
+    }
+
+    /**
+     * Finds all the inheritance data in which the given versioned mappable class is
+     * the sub type tole.
+     *
+     * @param subTypeVersionedMappableId The id of the versioned mappable class for which the inheritance data in sub type role will be looked up.
+     * @param pageable The pageable information in the request.
+     * @return All inheritance data which indicates that the given mappable in a game version is a sub type.
+     */
+    public Mono<Page<InheritanceDataDMO>> findAllForSubType(
+            final UUID subTypeVersionedMappableId,
+            final Pageable pageable
+    ) {
+        return createPagedStarRequest(
+                selectSpecWithJoin -> selectSpecWithJoin.withCriteria(where(reference("sub_type_versioned_mappable_id")).is(parameter(subTypeVersionedMappableId))),
+                pageable
+        );
+    }
+}
