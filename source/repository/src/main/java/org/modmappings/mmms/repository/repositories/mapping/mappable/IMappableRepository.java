c37a64fc10539683c4080aa6afd531f756be3966
Marc Hermans <marc.hermans@ldtteam.com>
2020-01-26 12:11:13 +0100

    Started port of MappableRepository
    Signed-off-by: Marc Hermans <marc.hermans@ldtteam.com>

--- source/repository/src/main/java/org/modmappings/mmms/repository/repositories/mapping/mappable/IMappableRepository.java
+++ source/repository/src/main/java/org/modmappings/mmms/repository/repositories/mapping/mappable/IMappableRepository.java
@@ -3,13 +3,18 @@ package org.modmappings.mmms.repository.repositories.mapping.mappable;
 import org.modmappings.mmms.repository.model.mapping.mappable.MappableDMO;
 import org.modmappings.mmms.repository.model.mapping.mappable.MappableTypeDMO;
 import org.modmappings.mmms.repository.repositories.ModMappingR2DBCRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Query;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.modmappings.mmms.er2dbc.data.statements.criteria.ColumnBasedCriteria.*;
 
 /**
  * Represents a repository which can provide and store {@link MappableDMO} objects.
  */
-public interface IMappableRepository extends ModMappingR2DBCRepository<MappableDMO> {
+public class MappableRepository extends ModMappingR2DBCRepository<MappableDMO> {
 
     /**
      * Finds all mappables which are of a given type.
@@ -19,7 +24,15 @@ public interface IMappableRepository extends ModMappingR2DBCRepository<MappableD
      * @return The mappables of the given type.
      */
     @Query("SELECT * FROM mappable m WHERE m.type = $1")
-    Flux<MappableDMO> findAllForType(MappableTypeDMO type);
+    Mono<Page<MappableDMO>> findAllByType(
+            final MappableTypeDMO type,
+            final Pageable pageable
+    ) {
+        return createPagedStarRequest(
+                selectSpecWithJoin -> selectSpecWithJoin.withCriteria(where(reference("type")).is(parameter(type))),
+                pageable
+        );
+    }
 
     @Override
     @Query("Select * from mappable m")
