2f4176887cf4e006fda88f96a90e20b15031ceea
Reece <reece@rjward.net>
2020-07-03 16:32:26 +0100

    Fix missing configure function

--- source/api/src/main/java/org/modmappings/mmms/api/configuration/WebSecurityConfiguration.java
+++ source/api/src/main/java/org/modmappings/mmms/api/configuration/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -79,6 +80,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             final JSONArray roleData = (JSONArray) authData.get("roles");
             return roleData.stream().map(roleObj -> (String) roleObj).map(roleStr -> "ROLE_" + roleStr).collect(Collectors.toSet());
         }
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
     }
 
     @Bean
