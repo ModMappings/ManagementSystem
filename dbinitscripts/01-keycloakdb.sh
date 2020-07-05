8395f823b1b9075873fe4fe9c441cbf7eaef6ea4
cpw <cpw+github@weeksfamily.ca>
2020-07-05 18:54:44 -0400

    why did this work locally?

--- dbinitscripts/01-keycloakdb.sh
+++ dbinitscripts/01-keycloakdb.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER keycloak;
+    CREATE USER keycloak with password 'keycloak';
     CREATE DATABASE keycloak;
     GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
 EOSQL
