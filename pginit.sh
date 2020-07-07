32795b67e9ee4b0f9262c0bafaa9ff65b9b5fd9a
cpw <cpw+github@weeksfamily.ca>
2020-07-06 21:52:58 -0400

    Such a fiddly arsed thing

--- pginit.sh
+++ pginit.sh
@@ -3,9 +3,10 @@
 # Cleanup and init of postgres
 
 docker stop mmms_postgres_1
+docker rm mmms_postgres_1
 docker volume rm -f mmms_postgresdb
 
-tmp=$(docker run -d -v mmms_postgresdb:/var/lib/postgresql/data -v ${pwd}/dbinitscripts/:/docker-entrypoint-initdb.d/ -e POSTGRES_PASSWORD=postgres postgres:12)
+tmp=$(docker run -d -v mmms_postgresdb:/var/lib/postgresql/data -v ${PWD}/dbinitscripts/:/docker-entrypoint-initdb.d/ -e POSTGRES_PASSWORD=postgres postgres:12)
 sleep 10
 docker stop $tmp
 docker logs $tmp
