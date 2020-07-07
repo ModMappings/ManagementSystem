99081ceb243a5be2f9705cf6f9a11e9a104d9259
cpw <cpw+github@weeksfamily.ca>
2020-07-06 21:43:30 -0400

    Actually wait for docker to finish, hopefully

--- pginit.sh
+++ pginit.sh
@@ -5,5 +5,8 @@
 docker stop mmms_postgres_1
 docker volume rm -f mmms_postgresdb
 
-tmp=$(docker run -d --rm -v mmms_postgresdb:/var/lib/postgresql/data -v ${pwd}/dbinitscripts/:/docker-entrypoint-initdb.d/ -e POSTGRES_PASSWD=postgres postgres:12)
+tmp=$(docker run -d -v mmms_postgresdb:/var/lib/postgresql/data -v ${pwd}/dbinitscripts/:/docker-entrypoint-initdb.d/ -e POSTGRES_PASSWORD=postgres postgres:12)
+sleep 10
 docker stop $tmp
+docker logs $tmp
+docker rm $tmp
