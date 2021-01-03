8e80d2532c535899ffe46baa2234eaa186e71d24
Marc Hermans <marc.hermans@ldtteam.com>
2021-01-03 18:49:34 +0100

    Adapt the mapping repository search query to not do ugly self joins.
    Signed-off-by: Marc Hermans <marc.hermans@ldtteam.com>

--- source/repository/src/main/java/org/modmappings/mmms/repository/repositories/mapping/mappings/mapping/MappingRepositoryImpl.java
+++ source/repository/src/main/java/org/modmappings/mmms/repository/repositories/mapping/mappings/mapping/MappingRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.modmappings.mmms.er2dbc.data.statements.criteria.ColumnBasedCriteria;
 import org.modmappings.mmms.er2dbc.data.statements.expression.Expressions;
 import org.modmappings.mmms.er2dbc.data.statements.mapper.ExtendedStatementMapper;
 import org.modmappings.mmms.er2dbc.data.statements.select.SelectSpecWithJoin;
+import org.modmappings.mmms.er2dbc.data.statements.sort.SortSpec;
 import org.modmappings.mmms.repository.model.mapping.mappable.MappableTypeDMO;
 import org.modmappings.mmms.repository.model.mapping.mappings.MappingDMO;
 import org.modmappings.mmms.repository.repositories.AbstractModMappingRepository;
@@ -22,8 +23,11 @@ import java.util.UUID;
 
 import static org.modmappings.mmms.er2dbc.data.statements.criteria.ColumnBasedCriteria.on;
 import static org.modmappings.mmms.er2dbc.data.statements.criteria.ColumnBasedCriteria.where;
-import static org.modmappings.mmms.er2dbc.data.statements.join.JoinSpec.join;
-import static org.modmappings.mmms.er2dbc.data.statements.join.JoinSpec.leftOuterJoin;
+import static org.modmappings.mmms.er2dbc.data.statements.expression.Expressions.reference;
+import static org.modmappings.mmms.er2dbc.data.statements.join.JoinSpec.*;
+import static org.modmappings.mmms.er2dbc.data.statements.sort.SortSpec.Order.asc;
+import static org.modmappings.mmms.er2dbc.data.statements.sort.SortSpec.Order.desc;
+import static org.modmappings.mmms.er2dbc.data.statements.sort.SortSpec.sort;
 
 /**
  * Represents a repository which can provide and store {@link MappingDMO} objects.
@@ -51,41 +55,47 @@ class MappingRepositoryImpl extends AbstractModMappingRepository<MappingDMO> imp
                                                      final boolean externallyVisibleOnly,
                                                      final Pageable pageable) {
         return createPagedStarRequest(
-                selectSpecWithJoin -> selectSpecWithJoin
-                        .join(() -> join("release_component", "rc")
-                                .on(() -> on(Expressions.reference("id")).is(Expressions.reference("rc", "mapping_id"))))
-                        .join(() -> join("versioned_mappable", "vm")
-                                .on(() -> on(Expressions.reference("versioned_mappable_id")).is(Expressions.reference("vm", "id"))))
-                        .join(() -> join("mappable", "mp")
-                                .on(() -> on(Expressions.reference("vm", "mappable_id")).is(Expressions.reference("mp", "id"))))
-                        .join(() -> join("mapping_type", "mt")
-                                .on(() -> on(Expressions.reference("mapping_type_id")).is(Expressions.reference("mt", "id"))))
-                        .join(() -> leftOuterJoin("mapping", "m2")
-                                .on(() -> on(Expressions.reference("versioned_mappable_id")).is(Expressions.reference("m2", "versioned_mappable_id"))
-                                        .and(Expressions.reference("mapping_type_id")).is(Expressions.reference("m2", "mapping_type_id"))
-                                        .and(Expressions.reference("created_on")).lessThan(Expressions.reference("m2", "created_on")))
-                        )
-                        .where(
-                                () -> {
-                                    ColumnBasedCriteria criteria = latestOnly ? where(Expressions.reference("m2", "id")).isNull() : null;
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, versionedMappableId, "", "versioned_mappable_id");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, releaseId, "rc", "release_id");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, mappableType, "mp", "type");
-                                    criteria = nonNullAndMatchesCheckForWhere(criteria, inputRegex, "", "input");
-                                    criteria = nonNullAndMatchesCheckForWhere(criteria, outputRegex, "", "output");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, mappingTypeId, "", "mapping_type_id");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, gameVersionId, "vm", "game_version_id");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, parentClassId, "vm", "parent_class_id");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, parentMethodId, "vm", "parent_method_id");
-                                    criteria = nonNullAndEqualsCheckForWhere(criteria, userId, "", "created_by");
+                selectSpecWithJoin -> {
+                    if (latestOnly) {
+                        selectSpecWithJoin = selectSpecWithJoin.withDistinctOn(reference("output"));
+                    }
 
-                                    if (externallyVisibleOnly) {
-                                        criteria = nonNullAndEqualsCheckForWhere(criteria, true, "mt", "visible");
+                    selectSpecWithJoin = selectSpecWithJoin
+                            .join(() -> optionalJoin(parentClassId != null || parentMethodId != null, "versioned_mappable", "vm")
+                                    .on(() -> on(reference("versioned_mappable_id")).is(reference("vm", "id")))
+                            )
+                            .join(() -> nonNullLeftOuterJoin(releaseId, "release_component", "rc")
+                                    .on(() -> on(reference("id")).is(reference("rc", "mapping_id"))))
+                            .join(() -> join("mapping_type", "mt")
+                                    .on(() -> on(reference("mapping_type_id")).is(reference("mt", "id"))))
+                            .where(
+                                    () -> {
+                                        ColumnBasedCriteria criteria = nonNullAndEqualsCheckForWhere(null, versionedMappableId, "", "versioned_mappable_id");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, releaseId, "rc", "release_id");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, mappableType, "", "mappable_type");
+                                        criteria = nonNullAndMatchesCheckForWhere(criteria, inputRegex, "", "input");
+                                        criteria = nonNullAndMatchesCheckForWhere(criteria, outputRegex, "", "output");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, mappingTypeId, "", "mapping_type_id");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, gameVersionId, "", "game_version_id");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, parentClassId, "vm", "parent_class_id");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, parentMethodId, "vm", "parent_method_id");
+                                        criteria = nonNullAndEqualsCheckForWhere(criteria, userId, "", "created_by");
+
+                                        if (externallyVisibleOnly) {
+                                            criteria = nonNullAndEqualsCheckForWhere(criteria, true, "mt", "visible");
+                                        }
+
+                                        return criteria;
                                     }
+                            );
 
-                                    return criteria;
-                                }
-                        )
+                    if (latestOnly)
+                    {
+                        selectSpecWithJoin = selectSpecWithJoin.withSort(sort(asc(reference("output"))).and(desc(reference("created_on"))));
+                    }
+
+                    return selectSpecWithJoin;
+                }
                 ,
                 pageable
         );
@@ -102,7 +112,7 @@ class MappingRepositoryImpl extends AbstractModMappingRepository<MappingDMO> imp
         final ExtendedStatementMapper mapper = getAccessStrategy().getStatementMapper().forType(this.getEntity().getJavaType());
         final SelectSpecWithJoin specWithJoin = mapper.createSelectWithJoin(this.getEntity().getTableName())
                 .withProjectionFromColumnName(columns)
-                .join(() -> join("mapping_type", "mt").on(() -> on(Expressions.reference("mapping_type_id")).is(Expressions.reference("mt", "id"))))
+                .join(() -> join("mapping_type", "mt").on(() -> on(reference("mapping_type_id")).is(reference("mt", "id"))))
                 .where(() -> {
                     ColumnBasedCriteria criteria = nonNullAndEqualsCheckForWhere(
                             null,
